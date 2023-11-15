013a6e16ed99faf6bc4472af7c0a2ef8c804f7ee
Md. Roman Islam Ali <romanislam897@gmail.com>
2023-11-15 12:11:18 +0600

    Websocket Configuration

--- notification-backend/src/main/java/com/roman/notificationbackend/WebSocketConfig.java
+++ notification-backend/src/main/java/com/roman/notificationbackend/WebSocketConfig.java
@@ -12,7 +12,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     }
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/websocket-example");
+        registry.addEndpoint("/websocket-example").withSockJS();
     }
 
 }
